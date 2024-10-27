9401ee11642ab5765cfbdbc4c4741cc82101d3fc
Gal Zafar <theycallmefachfach@gmail.com>
2024-10-27 11:59:55 +0200

    basic monaco-editor syntax highlighting

--- src/monaco.ts
+++ src/monaco.ts
@@ -1,14 +1,17 @@
 // Import specific functions or objects
+
+
+
 import * as monaco from 'monaco-editor';
+import { registerLatexLanguage } from './latexConfig.js'; // Adjust the path as needed
+
+// Register the LaTeX language with Monaco
+registerLatexLanguage();
 
 const containerElement = document.getElementById('container') as HTMLElement
-// Initialize the Monaco Editor
+// Initialize the Monaco Editor with LaTeX support
 monaco.editor.create(containerElement, {
-  value: `\frac{A}{B}`,
+  value: `\\title{Sample Document}\n\\author{Author Name}\n\\date{\\today}\n\n\\begin{document}\n\\section{Introduction}\nThis is a simple LaTeX example.\n\\end{document}`,
   language: 'latex',
-  theme: 'vs-dark',
-  options: {
-    fontSize: 14,
-    readOnly: false
-  }
+  theme: 'vs-dark'
 });
