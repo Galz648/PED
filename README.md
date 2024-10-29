01adbd616d12bd0be4efc32aefea04d5c9ddae58
Gal Zafar <theycallmefachfach@gmail.com>
2024-10-29 16:44:23 +0200

    WIP: state sync between MathLive math-field and monaco editor

--- README.md
+++ README.md
@@ -3,11 +3,12 @@
 
 
 
-## Features
+## Implemented Features
 * Basic LaTex syntax highlighting
 * Editor Snippet example
 
 
 ## TODO:
-* split the screen into two panes (Monaco-editor, Cortex livemath)
+* have the monaco editor reflect what is inside the mathlive block and vice versa.
+* split the screen into two panes (Monaco-editor, Cortex livemath) - DONE
 * make only the latex math sections to be rendered to livemath (To Be Determined)
--- src/monaco.ts
+++ src/monaco.ts
@@ -1,14 +1,15 @@
 import * as monaco from 'monaco-editor';
-import { registerLatexLanguage } from './latexConfig.js'; 
+import { registerLatexLanguage } from './latex_config.js'; 
 
 // Register the LaTeX language with Monaco
 registerLatexLanguage();
 
 const containerElement = document.getElementById('container') as HTMLElement
 // Initialize the Monaco Editor with LaTeX support
-monaco.editor.create(containerElement, {
+export const editor = monaco.editor.create(containerElement, {
   value: `\\title{Sample Document}\n\\author{Author Name}\n\\date{\\today}\n\n\\begin{document}\n\\section{Introduction}\nThis is a simple LaTeX example.\n\\end{document}\n\\frac{A}{B}`,
   language: 'latex',
   theme: 'vs-dark'
 });
+
 
